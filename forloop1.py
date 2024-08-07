a085d69157284e4bd39eec1dcc9f8ad91aa7cfd7
Rawspey <femirawlings@gmail.com>
2024-08-07 07:28:24 +0100

    forloop1.py

---
+++ forloop1.py
@@ -0,0 +1,12 @@
+for i in range(100):
+   print("We like Python's turtles!")
+
+for amonth in ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September' '...etc']:
+	print ("One of the months of the year is", amonth)
+
+for number in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
+	print (number)
+
+import math
+for number in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
+print (math.sqrt (number))
